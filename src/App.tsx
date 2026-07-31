3698cffec1c0bbeb7d52d5ee8191c8eb4f2c12f5
Regina Cupa <reginacupa@gmail.com>
2026-07-31 15:14:41 -0300

    alterações no viteconfig

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { AttendanceRoom } from '@/pages/AttendanceRoom'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chamado" element={<NewCall />} />
--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/RodoSOS/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(import.meta.dirname, './src'),
     },
   },
 })
